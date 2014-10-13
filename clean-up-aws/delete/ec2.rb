ee79b2b24854d60978ccf403fc600048f6755852
Aya Komuro <komuro@serverworks.co.jp>
2014-10-13 12:12:33 +0900

    EC2のENIとSG（VPCじゃないやつ）を削除するのを追加した。ちょっと分からない所があったので後で確認する

--- clean-up-aws/delete/ec2.rb
+++ clean-up-aws/delete/ec2.rb
@@ -24,14 +24,12 @@ end
 
 
 #Terminate instances
-
 ec2.instances.each do |instance|
   if instance.exists?
     puts "Terminate: #{instance.id}"
     instance.terminate
   end
 end
-
 
 
 #Delete Keypair
@@ -44,3 +42,22 @@ ec2.key_pairs.each do |keypair|
 end
 
 
+#delete Network Interfaces
+
+ec2.network_interfaces.each do | eni |
+  #以下見直す。trueになる理由を調べる
+  if eni.status.equal? "in_use"
+    puts "Delete ENI: #{eni.id}"
+    eni.delete
+  end
+end
+
+
+# delete ec2 security group。
+# VPCのSGは後で削除する
+ec2.security_groups.each do | sg |
+   if sg.name != "default" and !sg.vpc
+      puts "Delete Security Group: #{sg.id}"
+      sg.delete
+   end
+end
